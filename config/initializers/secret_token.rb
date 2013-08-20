fb9936014ffb95269129db84e75f4b1f25c729a8
Dmitry <budyachocheck@gmail.com>
2013-08-20 12:52:49 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Balalayka::Application.config.secret_key_base = 'be99df2486a554b45ec3c730512edba9b32a9278b91cb8b7e0f26ddf87a005a36d511d68aa8690dd76fe1316ac21cdd30ff92a8245fb1b3c446cf31e2bff05e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Balalayka::Application.config.session_store :cookie_store, key: '_balalayka_session'
