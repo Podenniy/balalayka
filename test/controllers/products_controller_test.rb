57382de6ce71edf0b913b1a27bb8d7009f71fa7c
Dmitry <budyachocheck@gmail.com>
2013-08-20 18:48:24 +0300

    validation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+      @update = {
+        title: 'Lorem Ipsum',
+        description: 'Wibbles are fun!',
+        image_url: 'lorem.jpg',
+        price:19.95
+      }
   end
 
   test "should get index" do
@@ -16,11 +22,10 @@ class ProductsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create product" do
+  test "should create product" do 
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
-
     assert_redirected_to product_path(assigns(:product))
   end
 
