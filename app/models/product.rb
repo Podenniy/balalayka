d1402c02a5214ae92e5171c58c54b99ca594543b
Dmitry <budyachocheck@gmail.com>
2013-08-21 17:59:13 +0300

    Store index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true, :length => {
+  	:minimum => 10,
+  	:message => "Должно быть 10 символов"
+  }
+
+  #validates :image_url, allow_blank: true, format: {
+  	#with: %r{\a(gif|jpg|png)$}i,
+  	#message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  #
 end
